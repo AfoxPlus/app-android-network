5ca102b7ec65f881fe504bfe62e07cdf8774beec
Valentin Mendoza <jmendozat13@gmail.com>
2023-06-25 01:06:53 -0500

    Added method get currency id

--- demo/src/main/java/com/afoxplus/network/demo/global/AppPropertiesDemo.kt
+++ demo/src/main/java/com/afoxplus/network/demo/global/AppPropertiesDemo.kt
@@ -2,7 +2,6 @@ package com.afoxplus.network.demo.global
 
 import com.afoxplus.network.demo.BuildConfig
 import com.afoxplus.network.global.AppProperties
-import java.util.UUID
 import javax.inject.Inject
 
 internal class AppPropertiesDemo @Inject constructor() : AppProperties {
@@ -10,11 +9,9 @@ internal class AppPropertiesDemo @Inject constructor() : AppProperties {
         return BuildConfig.DEBUG
     }
 
-    override fun getDeviceData(): String {
-        return ""
-    }
+    override fun getDeviceData(): String = "module network"
 
-    override fun getUserUUID(): String {
-        return UUID.randomUUID().toString()
-    }
+    override fun getUserUUID(): String = "6c4795bd-5a51-46e3-8cf2-3943d53ae271"
+
+    override fun getCurrencyID(): String = "61a18be00b6de1476436de41"
 }
