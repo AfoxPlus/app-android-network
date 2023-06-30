9263f958725adfa978a9c7cb33ba54d3bcfc751b
Carlos Osorio <hacybeyker@gmail.com>
2023-06-29 20:39:46 -0500

    se agrego soporte para chucker

--- module/src/main/java/com/afoxplus/network/di/NetworkRetrofitModule.kt
+++ module/src/main/java/com/afoxplus/network/di/NetworkRetrofitModule.kt
@@ -1,18 +1,24 @@
 package com.afoxplus.network.di
 
+import android.content.Context
 import com.afoxplus.network.api.AnnotationsHandlerInterceptor
 import com.afoxplus.network.api.RetrofitGenerator
 import com.afoxplus.network.api.UrlProvider
 import com.afoxplus.network.extensions.addUniqueInstanceInterceptor
 import com.afoxplus.network.global.AppProperties
+import com.chuckerteam.chucker.api.ChuckerCollector
+import com.chuckerteam.chucker.api.ChuckerInterceptor
+import com.chuckerteam.chucker.api.RetentionManager
 import dagger.Module
 import dagger.Provides
 import dagger.hilt.InstallIn
+import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
 import okhttp3.*
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.converter.gson.GsonConverterFactory
 import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
@@ -34,9 +40,11 @@ internal class NetworkRetrofitModule {
     fun providerOkHttpClient(
         httpLoggingInterceptor: HttpLoggingInterceptor,
         urlProvider: UrlProvider,
-        appProperties: AppProperties
+        appProperties: AppProperties,
+        chuckerInterceptor: ChuckerInterceptor,
     ): OkHttpClient {
         return OkHttpClient.Builder()
+            .addInterceptor(chuckerInterceptor)
             .addUniqueInstanceInterceptor(httpLoggingInterceptor)
             .addUniqueInstanceInterceptor(AnnotationsHandlerInterceptor(urlProvider, appProperties))
             .connectTimeout(60, TimeUnit.SECONDS)
@@ -55,6 +63,30 @@ internal class NetworkRetrofitModule {
         val loggingInterceptor = HttpLoggingInterceptor()
         loggingInterceptor.level = HttpLoggingInterceptor.Level.BODY
         return loggingInterceptor
+    }
+
+    @Singleton
+    @Provides
+    fun provideChuckCollector(@ApplicationContext context: Context): ChuckerCollector {
+        return ChuckerCollector(
+            context = context,
+            showNotification = true,
+            retentionPeriod = RetentionManager.Period.ONE_HOUR
+        )
+    }
+
+    @Singleton
+    @Provides
+    fun provideChuckInterceptor(
+        chuckerCollector: ChuckerCollector,
+        @ApplicationContext context: Context
+    ): ChuckerInterceptor {
+        return ChuckerInterceptor.Builder(context)
+            .collector(chuckerCollector)
+            .maxContentLength(250_000L)
+            .redactHeaders("Auth-Token", "Bearer")
+            .alwaysReadResponseBody(true)
+            .build()
     }
 
 }
