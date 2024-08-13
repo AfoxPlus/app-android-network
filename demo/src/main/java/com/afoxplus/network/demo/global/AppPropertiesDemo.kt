81f9d09c0f4c37354b41d083cef7b8a71c9b4716
Valentin Mendoza <jmendozat13@gmail.com>
2024-08-13 12:59:46 -0500

    Update versions

--- demo/src/main/java/com/afoxplus/network/demo/global/AppPropertiesDemo.kt
+++ demo/src/main/java/com/afoxplus/network/demo/global/AppPropertiesDemo.kt
@@ -1,12 +1,11 @@
 package com.afoxplus.network.demo.global
 
-import com.afoxplus.network.demo.BuildConfig
 import com.afoxplus.network.global.AppProperties
 import javax.inject.Inject
 
 internal class AppPropertiesDemo @Inject constructor() : AppProperties {
     override fun isAppDebug(): Boolean {
-        return BuildConfig.DEBUG
+        return true
     }
 
     override fun getDeviceData(): String = "module network"
